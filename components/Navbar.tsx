6f139df5dfa3a2c5ad960448483d9a1cccf893bb
Weston <dogooddowell@protonmail.com>
2023-07-09 14:16:01 -0500

    Update Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
         <Container maxW={"1440px"} py={8}>
             <Flex flexDirection={"row"} justifyContent={"space-between"} alignItems={"center"}>
                 <Link href={"/"}>
-                    <Text fontSize={"xl"} fontWeight={"bold"}>Raffle App</Text>
+                    <Text fontSize={"xl"} fontWeight={"bold"}>NFTEarth Raffle</Text>
                 </Link>
                 <Flex flexDirection={"row"} alignItems={"center"}>
                     {!adminLoading && address === admin && (
