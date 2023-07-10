647438a0f98a7696273e4ee927cb2c1592cb2d82
Weston Nelson <dogooddowell@protonmail.com>
2023-07-09 22:26:49 -0500

    Update addresses.ts
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- const/addresses.ts
+++ const/addresses.ts
@@ -1,4 +1,4 @@
 export const LOTTERY_CONTRACT_ADDRESS = '0xFdd17F693230205e253022b68347e4196bc3A4fe';
 
-export const HERO_IMAGE_URL = 'ipfs://QmRHfw5gCqxLtLRN5JKnpFc5NKD9PjnhkeHSsPWx3cFUEB/NFTE.svg';
+export const HERO_IMAGE_URL = 'https://nftstorage.link/ipfs/bafkreibk6aq3ez3ewlecaivt4ssiopxqsio4rtgmsx72ho5odskvdlfyam';
 
